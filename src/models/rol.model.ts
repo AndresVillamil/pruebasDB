11ac9394b62f5b7d979286e751fc30c9c3ea2967
AndresVillamil <bcl.evillamil@gmail.com>
2021-11-10 19:25:13 -0500

    Prueba 1

---
+++ src/models/rol.model.ts
@@ -0,0 +1,38 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Rol extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  rol: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  descripcion: string;
+
+  @property({
+    type: 'string',
+  })
+  usuarioId?: string;
+
+  constructor(data?: Partial<Rol>) {
+    super(data);
+  }
+}
+
+export interface RolRelations {
+  // describe navigational properties here
+}
+
+export type RolWithRelations = Rol & RolRelations;
---
+++ src/repositories/usuario.repository.ts
@@ -0,0 +1,22 @@
+import {inject, Getter} from '@loopback/core';
+import {DefaultCrudRepository, repository, HasOneRepositoryFactory} from '@loopback/repository';
+import {MongoDbDataSource} from '../datasources';
+import {Usuario, UsuarioRelations, Rol} from '../models';
+import {RolRepository} from './rol.repository';
+
+export class UsuarioRepository extends DefaultCrudRepository<
+  Usuario,
+  typeof Usuario.prototype.id,
+  UsuarioRelations
+> {
+
+  public readonly rol: HasOneRepositoryFactory<Rol, typeof Usuario.prototype.id>;
+
+  constructor(
+    @inject('datasources.mongoDB') dataSource: MongoDbDataSource, @repository.getter('RolRepository') protected rolRepositoryGetter: Getter<RolRepository>,
+  ) {
+    super(Usuario, dataSource);
+    this.rol = this.createHasOneRepositoryFactoryFor('rol', rolRepositoryGetter);
+    this.registerInclusionResolver('rol', this.rol.inclusionResolver);
+  }
+}
